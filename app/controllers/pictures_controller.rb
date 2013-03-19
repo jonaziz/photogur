3127a77de9f521f8bbc7cbc94b35ca06d84ba347
Jon Aziz <jonaziz@gmail.com>
2013-03-19 14:19:18 -0400

    Initial commit of my first rails project

---
+++ app/controllers/pictures_controller.rb
@@ -0,0 +1,7 @@
+class PicturesController < ActionController::Base
+
+	# Most methods inside controllers are called actions
+	def index
+		
+	end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Photogur::Application.config.secret_token = '15eacfa7bdb83a6c7ec057e3f42c838fb1afe8247c9573b9cf22523eafc549d67df51d80f6ff93ef4ad66477d202f228fccf3bbadb3fa8c65f1cefc498c97574'
