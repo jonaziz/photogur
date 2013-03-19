fd66d52e512eb9f64f2ced0a5e5aef358b52fadf
Jon Aziz <jonaziz@gmail.com>
2013-03-19 15:31:57 -0400

    Adding image sample and styling basics

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,7 +1,13 @@
-class PicturesController < ActionController::Base
+class PicturesController < ApplicationController
 
 	# Most methods inside controllers are called actions
 	def index
 		@greeting = "Hello World"
+
+		@pictures = [
+			'http://cdn4.leftlanenews.com/photos/content/january2013/thumbnails5.0/ford-f-150-review-ri_663.jpg',
+			'http://cdn4.leftlanenews.com/photos/content/january2013/thumbnails5.0/mercedes-cla45-leak-ri_300.jpg',
+			'http://cdn4.leftlanenews.com/photos/content/january2013/thumbnails5.0/fiskerrestart_300.jpg'
+		]
 	end
 end
