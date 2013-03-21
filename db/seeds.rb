d7d1ca223641ff94f94591756b20be5eeec84dee
Jon Aziz <jonaziz@gmail.com>
2013-03-21 09:01:24 -0400

    New form utilizing helper, modified seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,28 @@
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Emanuel', :city => cities.first)
 
-Picture.create(:artist => 'Sergey Ershov', :title => "The old church on the coast of White sea", :url => "http://monicao.s3.amazonaws.com/bitmaker/house.jpg")
-Picture.create(:artist => 'Stephen Scullion', :title => "Sea Power", :url => "http://monicao.s3.amazonaws.com/bitmaker/wave.jpg")
-Picture.create(:artist => 'John Wilhelm', :title => "Into the Poppies", :url => "http://monicao.s3.amazonaws.com/bitmaker/girl.jpg")
+# This create method can be called in the Rails console. This file is
+# used to enter sampple data into the DB
+
+# Alt steps:
+## db:drop
+## db:create
+## db:migrate
+## db:seed
+
+Picture.create(
+	:artist => 'Sergey Ershov',
+	:title => "The old church on the coast of White sea",
+	:url => "http://monicao.s3.amazonaws.com/bitmaker/house.jpg"
+)
+
+Picture.create(
+	:artist => 'Stephen Scullion'
+	:title => "Sea Power"
+	:url => "http://monicao.s3.amazonaws.com/bitmaker/wave.jpg"
+)
+Picture.create(
+	:artist => 'John Wilhelm',
+	:title => "Into the Poppies",
+	:url => "http://monicao.s3.amazonaws.com/bitmaker/girl.jpg"
+)
